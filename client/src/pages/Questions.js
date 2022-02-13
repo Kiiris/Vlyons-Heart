e6af4eee46439746a5decc9c6b68c9d1b92b204e
Kiiris <barthlemont@gmail.com>
2022-02-13 09:11:59 -0500

    question functionality finished

--- client/src/pages/Questions.js
+++ client/src/pages/Questions.js
@@ -1,60 +1,358 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Questions = (props) => {
-  const colors = ['#0088FE', '#00C49F', '#FFBB28'];
-  const [index, setIndex] = useState(0);
-  const [questions, setQuestions] = useState([
-    {
-      athletic: 'Are your physical attributes more important than mental?',
-      athletic_two: 'Is working out a part of your normal routine?'
-    },
-    {
-      definitely: 'Definitely',
-      somewhat: 'somewhat',
-      moderately: 'moderately',
-      not_at_all: 'not at all'
-    }
-  ]);
+  const [clickDef, setDef] = useState(false);
+  const [clickSome, setSome] = useState(false);
+  const [clickMod, setMod] = useState(false);
+  const [clickNot, setNot] = useState(false);
 
-  const romance = [
-    "Is long distance something that you're comfortable with?",
-    'How romantic do you think you are?',
-    'Life is good?'
-  ];
+  const questions = {
+    romantic: "Is long distance something that you're comfortable with?",
+    romantic: 'How romantic do you think you are?',
+    romantic: 'Life is good?',
+    nerdy: 'How important are games to you?',
+    nerdy: "Is coding something you're very particular about?",
+    hardworking: 'Are your physical attributes more important than mental?',
+    hardworking: 'Is working out a part of your normal routine?',
+    musical: 'Do you listen to a lot music during the day?'
+  };
+
+  const values = Object.values(questions);
+
   const [text, setText] = useState(
-    romance[Math.floor(Math.random() * romance.length)]
+    values[Math.floor(Math.random() * values.length)]
   );
-  let randomItem = romance[Math.floor(Math.random() * romance.length)];
+  let randomItem = values[Math.floor(Math.random() * values.length)];
   const changeText = (text) => {
     setText(text);
+    const key = Object.keys(questions).find((key) => questions[key] === text);
+    console.log(key);
   };
-  const nerdy = {
-    'How important are games to you?': 2,
-    "Is coding something you're very particular about?": 2
+  const Check = () => {
+    setDef(true);
+    setMod(false);
+    setNot(false);
+    setSome(false);
   };
-  const another = ['pickles', 'sheep', 'pie'];
-  const Check = (e) => {
-    if (def.current.id == 'definitely') {
-      console.log('success');
-    } else if (mod.current.id == 'moderately') {
-      console.log('moderate success');
-    } else if (some.current.id == 'somewhat') {
-      console.log('some success');
+  const CheckSome = () => {
+    setSome(true);
+    setDef(false);
+    setMod(false);
+    setNot(false);
+  };
+  const CheckMod = () => {
+    setMod(true);
+    setDef(false);
+    setNot(false);
+    setSome(false);
+  };
+  const CheckNot = () => {
+    setNot(true);
+    setDef(false);
+    setMod(false);
+    setSome(false);
+  };
+
+  const key = Object.keys(questions).find((key) => questions[key] === text);
+
+  const handleSubmit = async () => {
+    if ((clickDef === true) & (key === 'hardworking')) {
+      await axios.put(`http://localhost:8000/user/${props.currentUser.id}`, {
+        email: props.currentUser.email,
+        username: props.currentUser.username,
+        password: props.currentUser.password,
+        gender: props.currentUser.gender,
+        preference: props.currentUser.preference,
+        birth_year: props.currentUser.birth_year,
+        location: props.currentUser.location,
+        description: props.currentUser.description,
+        photo_url: props.currentUser.photo_url,
+        likes: props.currentUser.liked_by,
+        liked_by: props.currentUser.liked_by,
+        hardworking: parseInt((props.currentUser.hardworking += 4)),
+        romantic: props.currentUser.romantic,
+        musical: props.currentUser.musical,
+        nerdy: props.currentUser.nerdy
+      });
+    } else if ((clickSome === true) & (key === 'hardworking')) {
+      await axios.put(`http://localhost:8000/user/${props.currentUser.id}`, {
+        email: props.currentUser.email,
+        username: props.currentUser.username,
+        password: props.currentUser.password,
+        gender: props.currentUser.gender,
+        preference: props.currentUser.preference,
+        birth_year: props.currentUser.birth_year,
+        location: props.currentUser.location,
+        description: props.currentUser.description,
+        photo_url: props.currentUser.photo_url,
+        likes: props.currentUser.liked_by,
+        liked_by: props.currentUser.liked_by,
+        hardworking: parseInt((props.currentUser.hardworking += 3)),
+        romantic: props.currentUser.romantic,
+        musical: props.currentUser.musical,
+        nerdy: props.currentUser.nerdy
+      });
+    } else if ((clickMod === true) & (key === 'hardworking')) {
+      await axios.put(`http://localhost:8000/user/${props.currentUser.id}`, {
+        email: props.currentUser.email,
+        username: props.currentUser.username,
+        password: props.currentUser.password,
+        gender: props.currentUser.gender,
+        preference: props.currentUser.preference,
+        birth_year: props.currentUser.birth_year,
+        location: props.currentUser.location,
+        description: props.currentUser.description,
+        photo_url: props.currentUser.photo_url,
+        likes: props.currentUser.liked_by,
+        liked_by: props.currentUser.liked_by,
+        hardworking: parseInt((props.currentUser.hardworking += 2)),
+        romantic: props.currentUser.romantic,
+        musical: props.currentUser.musical,
+        nerdy: props.currentUser.nerdy
+      });
+    } else if ((clickNot === true) & (key === 'hardworking')) {
+      await axios.put(`http://localhost:8000/user/${props.currentUser.id}`, {
+        email: props.currentUser.email,
+        username: props.currentUser.username,
+        password: props.currentUser.password,
+        gender: props.currentUser.gender,
+        preference: props.currentUser.preference,
+        birth_year: props.currentUser.birth_year,
+        location: props.currentUser.location,
+        description: props.currentUser.description,
+        photo_url: props.currentUser.photo_url,
+        likes: props.currentUser.liked_by,
+        liked_by: props.currentUser.liked_by,
+        hardworking: parseInt((props.currentUser.hardworking += 1)),
+        romantic: props.currentUser.romantic,
+        musical: props.currentUser.musical,
+        nerdy: props.currentUser.nerdy
+      });
+    } else if ((clickDef === true) & (key === 'nerdy')) {
+      await axios.put(`http://localhost:8000/user/${props.currentUser.id}`, {
+        email: props.currentUser.email,
+        username: props.currentUser.username,
+        password: props.currentUser.password,
+        gender: props.currentUser.gender,
+        preference: props.currentUser.preference,
+        birth_year: props.currentUser.birth_year,
+        location: props.currentUser.location,
+        description: props.currentUser.description,
+        photo_url: props.currentUser.photo_url,
+        likes: props.currentUser.liked_by,
+        liked_by: props.currentUser.liked_by,
+        hardworking: props.currentUser.hardworking,
+        romantic: props.currentUser.romantic,
+        musical: props.currentUser.musical,
+        nerdy: parseInt((props.currentUser.nerdy += 4))
+      });
+    } else if ((clickSome === true) & (key === 'nerdy')) {
+      await axios.put(`http://localhost:8000/user/${props.currentUser.id}`, {
+        email: props.currentUser.email,
+        username: props.currentUser.username,
+        password: props.currentUser.password,
+        gender: props.currentUser.gender,
+        preference: props.currentUser.preference,
+        birth_year: props.currentUser.birth_year,
+        location: props.currentUser.location,
+        description: props.currentUser.description,
+        photo_url: props.currentUser.photo_url,
+        likes: props.currentUser.liked_by,
+        liked_by: props.currentUser.liked_by,
+        hardworking: props.currentUser.hardworking,
+        romantic: props.currentUser.romantic,
+        musical: props.currentUser.musical,
+        nerdy: parseInt((props.currentUser.nerdy += 3))
+      });
+    } else if ((clickMod === true) & (key === 'nerdy')) {
+      await axios.put(`http://localhost:8000/user/${props.currentUser.id}`, {
+        email: props.currentUser.email,
+        username: props.currentUser.username,
+        password: props.currentUser.password,
+        gender: props.currentUser.gender,
+        preference: props.currentUser.preference,
+        birth_year: props.currentUser.birth_year,
+        location: props.currentUser.location,
+        description: props.currentUser.description,
+        photo_url: props.currentUser.photo_url,
+        likes: props.currentUser.liked_by,
+        liked_by: props.currentUser.liked_by,
+        hardworking: props.currentUser.hardworking,
+        romantic: props.currentUser.romantic,
+        musical: props.currentUser.musical,
+        nerdy: parseInt((props.currentUser.nerdy += 2))
+      });
+    } else if ((clickNot === true) & (key === 'nerdy')) {
+      await axios.put(`http://localhost:8000/user/${props.currentUser.id}`, {
+        email: props.currentUser.email,
+        username: props.currentUser.username,
+        password: props.currentUser.password,
+        gender: props.currentUser.gender,
+        preference: props.currentUser.preference,
+        birth_year: props.currentUser.birth_year,
+        location: props.currentUser.location,
+        description: props.currentUser.description,
+        photo_url: props.currentUser.photo_url,
+        likes: props.currentUser.liked_by,
+        liked_by: props.currentUser.liked_by,
+        hardworking: props.currentUser.hardworking,
+        romantic: props.currentUser.romantic,
+        musical: props.currentUser.musical,
+        nerdy: parseInt((props.currentUser.nerdy += 1))
+      });
+    } else if ((clickDef === true) & (key === 'romantic')) {
+      await axios.put(`http://localhost:8000/user/${props.currentUser.id}`, {
+        email: props.currentUser.email,
+        username: props.currentUser.username,
+        password: props.currentUser.password,
+        gender: props.currentUser.gender,
+        preference: props.currentUser.preference,
+        birth_year: props.currentUser.birth_year,
+        location: props.currentUser.location,
+        description: props.currentUser.description,
+        photo_url: props.currentUser.photo_url,
+        likes: props.currentUser.liked_by,
+        liked_by: props.currentUser.liked_by,
+        hardworking: props.currentUser.hardworking,
+        romantic: parseInt((props.currentUser.romantic += 4)),
+        musical: props.currentUser.musical,
+        nerdy: props.currentUser.nerdy
+      });
+    } else if ((clickSome === true) & (key === 'romantic')) {
+      await axios.put(`http://localhost:8000/user/${props.currentUser.id}`, {
+        email: props.currentUser.email,
+        username: props.currentUser.username,
+        password: props.currentUser.password,
+        gender: props.currentUser.gender,
+        preference: props.currentUser.preference,
+        birth_year: props.currentUser.birth_year,
+        location: props.currentUser.location,
+        description: props.currentUser.description,
+        photo_url: props.currentUser.photo_url,
+        likes: props.currentUser.liked_by,
+        liked_by: props.currentUser.liked_by,
+        romantic: parseInt((props.currentUser.romantic += 3)),
+        hardworking: props.currentUser.hardworking,
+        musical: props.currentUser.musical,
+        nerdy: props.currentUser.nerdy
+      });
+    } else if ((clickMod === true) & (key === 'romantic')) {
+      await axios.put(`http://localhost:8000/user/${props.currentUser.id}`, {
+        email: props.currentUser.email,
+        username: props.currentUser.username,
+        password: props.currentUser.password,
+        gender: props.currentUser.gender,
+        preference: props.currentUser.preference,
+        birth_year: props.currentUser.birth_year,
+        location: props.currentUser.location,
+        description: props.currentUser.description,
+        photo_url: props.currentUser.photo_url,
+        likes: props.currentUser.liked_by,
+        liked_by: props.currentUser.liked_by,
+        romantic: parseInt((props.currentUser.romantic += 2)),
+        hardworking: props.currentUser.hardworking,
+        musical: props.currentUser.musical,
+        nerdy: props.currentUser.nerdy
+      });
+    } else if ((clickNot === true) & (key === 'romantic')) {
+      await axios.put(`http://localhost:8000/user/${props.currentUser.id}`, {
+        email: props.currentUser.email,
+        username: props.currentUser.username,
+        password: props.currentUser.password,
+        gender: props.currentUser.gender,
+        preference: props.currentUser.preference,
+        birth_year: props.currentUser.birth_year,
+        location: props.currentUser.location,
+        description: props.currentUser.description,
+        photo_url: props.currentUser.photo_url,
+        likes: props.currentUser.liked_by,
+        liked_by: props.currentUser.liked_by,
+        romantic: parseInt((props.currentUser.romantic += 1)),
+        hardworking: props.currentUser.hardworking,
+        musical: props.currentUser.musical,
+        nerdy: props.currentUser.nerdy
+      });
+    } else if ((clickDef === true) & (key === 'musical')) {
+      await axios.put(`http://localhost:8000/user/${props.currentUser.id}`, {
+        email: props.currentUser.email,
+        username: props.currentUser.username,
+        password: props.currentUser.password,
+        gender: props.currentUser.gender,
+        preference: props.currentUser.preference,
+        birth_year: props.currentUser.birth_year,
+        location: props.currentUser.location,
+        description: props.currentUser.description,
+        photo_url: props.currentUser.photo_url,
+        likes: props.currentUser.liked_by,
+        liked_by: props.currentUser.liked_by,
+        hardworking: props.currentUser.hardworking,
+        romantic: props.currentUser.romantic,
+        musical: parseInt((props.currentUser.musical += 4)),
+        nerdy: props.currentUser.nerdy
+      });
+    } else if ((clickSome === true) & (key === 'musical')) {
+      await axios.put(`http://localhost:8000/user/${props.currentUser.id}`, {
+        email: props.currentUser.email,
+        username: props.currentUser.username,
+        password: props.currentUser.password,
+        gender: props.currentUser.gender,
+        preference: props.currentUser.preference,
+        birth_year: props.currentUser.birth_year,
+        location: props.currentUser.location,
+        description: props.currentUser.description,
+        photo_url: props.currentUser.photo_url,
+        likes: props.currentUser.liked_by,
+        liked_by: props.currentUser.liked_by,
+        romantic: props.currentUser.romantic,
+        hardworking: props.currentUser.hardworking,
+        musical: parseInt((props.currentUser.musical += 3)),
+        nerdy: props.currentUser.nerdy
+      });
+    } else if ((clickMod === true) & (key === 'musical')) {
+      await axios.put(`http://localhost:8000/user/${props.currentUser.id}`, {
+        email: props.currentUser.email,
+        username: props.currentUser.username,
+        password: props.currentUser.password,
+        gender: props.currentUser.gender,
+        preference: props.currentUser.preference,
+        birth_year: props.currentUser.birth_year,
+        location: props.currentUser.location,
+        description: props.currentUser.description,
+        photo_url: props.currentUser.photo_url,
+        likes: props.currentUser.liked_by,
+        liked_by: props.currentUser.liked_by,
+        romantic: props.currentUser.romantic,
+        hardworking: props.currentUser.hardworking,
+        musical: parseInt((props.currentUser.musical += 2)),
+        nerdy: props.currentUser.nerdy
+      });
+    } else if ((clickNot === true) & (key === 'musical')) {
+      await axios.put(`http://localhost:8000/user/${props.currentUser.id}`, {
+        email: props.currentUser.email,
+        username: props.currentUser.username,
+        password: props.currentUser.password,
+        gender: props.currentUser.gender,
+        preference: props.currentUser.preference,
+        birth_year: props.currentUser.birth_year,
+        location: props.currentUser.location,
+        description: props.currentUser.description,
+        photo_url: props.currentUser.photo_url,
+        likes: props.currentUser.liked_by,
+        liked_by: props.currentUser.liked_by,
+        romantic: props.currentUser.romantic,
+        hardworking: props.currentUser.hardworking,
+        musical: parseInt((props.currentUser.musical += 1)),
+        nerdy: props.currentUser.nerdy
+      });
     }
   };
-  const def = useRef('definitely');
-  const mod = useRef('moderately');
-  const some = useRef('somewhat');
-  console.log(mod.current.id);
-  console.log(def.current.id);
-  console.log(some.current.id);
   return (
     <div className="slideshow">
       <section>
         <h1>{text}</h1>
         <button
-          ref={def}
-          id="definitely"
+          className={clickDef ? 'definitely' : 'inactive'}
           onClick={() => {
             changeText(randomItem);
             Check();
@@ -64,35 +362,40 @@ const Questions = (props) => {
         </button>
         <hr />
         <button
-          id="somewhat"
-          ref={some}
+          className={clickSome ? 'somewhat' : 'inactive'}
           onClick={() => {
             changeText(randomItem);
-            Check();
+            CheckSome();
           }}
         >
           Somewhat
         </button>
         <hr />
         <button
-          ref={mod}
-          id="moderately"
-          // ref={myRef}
+          className={clickMod ? 'moderately' : 'inactive'}
           onClick={() => {
             changeText(randomItem);
-            Check();
+            CheckMod();
           }}
         >
           Moderately
         </button>
+
         <hr />
         <button
+          className={clickNot ? 'not' : 'inactive'}
           onClick={() => {
             changeText(randomItem);
+            CheckNot();
           }}
         >
           Not at all
         </button>
+        <button
+          className="nicebutton"
+          disabled={!clickDef & clickMod & clickNot & clickSome}
+          onClick={handleSubmit}
+        ></button>
       </section>
     </div>
   );
