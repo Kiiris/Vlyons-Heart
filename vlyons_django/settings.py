2e0ffdc12aad4fc0e370bcf8953ac6bb3ccd2ff6
Kiiris <barthlemont@gmail.com>
2022-02-13 23:35:53 -0500

    trying to deploy again

--- vlyons_django/settings.py
+++ vlyons_django/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.getenv('MODE') == 'dev' else False
+DEBUG = True if os.getenv['MODE'] == 'dev' else False
 
 ALLOWED_HOSTS = ['*']
 
