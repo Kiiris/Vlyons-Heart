06ea9b5ec64856b7e7c1754720b70e4d43a27241
Kiiris <barthlemont@gmail.com>
2022-02-13 23:44:25 -0500

    changes to settings.py

--- vlyons_django/settings.py
+++ vlyons_django/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.getenv['MODE'] == 'dev' else False
+DEBUG = True if os.getenv('MODE') == 'dev' else False
 
 ALLOWED_HOSTS = ['*']
 
